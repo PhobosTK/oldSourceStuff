1665db05ec8ed6dac38f43e0459f44514cf21a06
PhobosTK <riseoftechnology@gmail.com>
2023-03-05 00:19:06 +0000

    Create dec2hex.c

---
+++ cStuffDeitel/dec2hex.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Input the number to convert: ");
+    scanf("%d" , &num);
+    printf("%x" , num);
+    return 0;
